13b17115da090a4839a5a4c89b747ae6534cb6c9
Frangen, Leon <lesoftware@proton.me>
2024-03-31 19:59:08 +0200

    remove cursor from test; use provided HTTP-Request test string

--- src/request/head/test.rs
+++ src/request/head/test.rs
@@ -3,7 +3,7 @@ use std::borrow::Cow;
 use crate::request::head::RequestHead;
 use futures::executor::block_on;
 use futures::io::Cursor;
-use http::{HeaderMap, Method, Uri, Version};
+use http::{Method, Version, Uri, HeaderMap};
 
 use super::parse::RequestHeadParse;
 
@@ -37,31 +37,15 @@ fn test() {
 
 #[test]
 fn test_request_head_parse() {
-    let uri = Uri::builder()
-        .scheme("https")
-        .authority("google.com")
-        .path_and_query("/")
-        .build()
-        .unwrap();
+    let req_head = block_on( async {RequestHead::decode(INPUT).await.unwrap().1});
+    let mut req_head_parse = RequestHeadParse::new(8096,req_head.headers.len());
 
-    let header_map = HeaderMap::new();
-
-    let req_head = RequestHead::new(
-        Method::GET,
-        Cow::Borrowed(&uri),
-        Version::HTTP_11,
-        Cow::Borrowed(&header_map),
-    );
-
-    let mut req_head_parse = RequestHeadParse::new(8096, header_map.len());
-
-    let mut cursor = std::io::Cursor::new(req_head.to_vec().unwrap());
-
-    let _ = req_head_parse.read_data(&mut cursor);
+    let _ = req_head_parse.read_data(&mut req_head.to_vec().unwrap().as_slice());
     let req_header_recv = req_head_parse.try_take_head().unwrap();
 
     assert_eq!(req_head.method, req_header_recv.method);
     assert_eq!(req_head.uri, req_header_recv.uri);
     assert_eq!(req_head.version, req_header_recv.version);
     assert_eq!(req_head.headers, req_header_recv.headers);
+
 }
