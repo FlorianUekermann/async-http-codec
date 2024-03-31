11ba5bf4dbfd5b35df499272c33751f9aa7dc0d2
Frangen, Leon <lesoftware@proton.me>
2024-03-31 18:26:12 +0200

    Implement RequestHeaderParse

---
+++ src/request/head/parse.rs
@@ -0,0 +1,102 @@
+use std::io::Read;
+use crate::RequestHead;
+use http::header::HeaderName;
+use http::{HeaderValue, Method, Request, Uri, Version};
+use std::io;
+use std::io::ErrorKind::InvalidData;
+
+enum TerminatorOverlapInBuffer {
+    None,
+    Partial(usize),
+    Complete
+}
+
+pub struct RequestHeadParse {
+    buffer: Vec<u8>,
+    terminator: TerminatorOverlapInBuffer,
+    max_headers: usize,
+}
+
+impl<'a> RequestHeadParse {
+    const END: &'a[u8] = b"\r\n\r\n";
+    pub fn new(
+        max_buffer: usize,
+        max_headers: usize,
+    ) -> Self {
+        Self {
+            buffer: Vec::with_capacity(max_buffer),
+	    terminator: TerminatorOverlapInBuffer::None,
+            max_headers,
+        }
+    }
+    pub fn read_data<T: Read>(&mut self, rd: &mut T) -> Result<usize, std::io::Error> {
+	let mut chunks = [0u8;Self::END.len()];
+	loop {
+	    match self.terminator {
+		TerminatorOverlapInBuffer::None => {
+		    rd.read_exact(&mut chunks)?;
+		    self.buffer.extend_from_slice(&chunks);
+		    if chunks == Self::END {
+			self.terminator = TerminatorOverlapInBuffer::Complete;
+		    } else {
+			for i in 1..Self::END.len() {
+			    let window_size = Self::END.len() - i;
+			    if chunks[i..] == Self::END[0..window_size]{
+				self.terminator = TerminatorOverlapInBuffer::Partial(i);
+			    }
+			}
+		    }
+		},
+		TerminatorOverlapInBuffer::Complete => {
+		    break;
+		},
+		TerminatorOverlapInBuffer::Partial(x) => {
+		    let chunks = &mut chunks[0..x];
+		    rd.read_exact(chunks)?;
+		    self.buffer.extend_from_slice(chunks);
+		    match chunks[0..x] == Self::END[(Self::END.len()-x)..] {
+			true => self.terminator = TerminatorOverlapInBuffer::Complete,
+			false => self.terminator = TerminatorOverlapInBuffer::None
+		    }
+		}	    
+	    }
+	};
+	Ok(self.buffer.len())
+    }
+    pub fn try_take_head(&mut self) -> io::Result<RequestHead> {
+	let mut headers = vec![httparse::EMPTY_HEADER; self.max_headers];
+	let mut parsed_request = httparse::Request::new(&mut headers);
+	if parsed_request
+            .parse(self.buffer.as_ref())
+            .map_err(|err| io::Error::new(InvalidData, err.to_string()))?
+            .is_partial()
+	{
+            return Err(io::Error::new(InvalidData, "malformed HTTP head"));
+	}
+	if parsed_request.version != Some(1) {
+            return Err(io::Error::new(InvalidData, "unsupported HTTP version"));
+	}
+	let method = Method::from_bytes(parsed_request.method.unwrap_or("").as_bytes())
+            .map_err(|err| io::Error::new(InvalidData, err.to_string()))?;
+	let uri = parsed_request
+            .path
+            .unwrap_or("")
+            .parse::<Uri>()
+            .map_err(|_| io::Error::new(InvalidData, "invalid uri"))?;
+	let mut request = Request::new(());
+	*request.method_mut() = method;
+	*request.uri_mut() = uri;
+	*request.version_mut() = Version::HTTP_11;
+	let headers = request.headers_mut();
+	headers.reserve(parsed_request.headers.len());
+	for header in parsed_request.headers {
+	    headers.append(
+		HeaderName::from_bytes(header.name.as_bytes())
+                    .map_err(|_| io::Error::new(InvalidData, "invalid header name"))?,
+		HeaderValue::from_bytes(header.value)
+                    .map_err(|_| io::Error::new(InvalidData, "invalid header value"))?,
+            );
+	}
+	Ok(RequestHead::from(request))
+    }
+}
--- src/request/head/test.rs
+++ src/request/head/test.rs
@@ -1,7 +1,11 @@
+use std::borrow::Cow;
+
 use crate::request::head::RequestHead;
 use futures::executor::block_on;
 use futures::io::Cursor;
-use http::{Method, Version};
+use http::{Method, Version, Uri, HeaderMap};
+
+use super::parse::RequestHeadParse;
 
 const INPUT: &[u8] = b"GET / HTTP/1.1\r\nhost: www.example.com\r\nconnection: close\r\n\r\n";
 
@@ -29,4 +33,26 @@ fn test() {
             String::from_utf8(INPUT.to_vec())
         );
     })
+}
+
+#[test]
+fn test_request_head_parse() {
+    let uri = Uri::builder().scheme("https").authority("google.com").path_and_query("/").build().unwrap();
+    
+    let header_map = HeaderMap::new();
+    
+    let req_head = RequestHead::new(Method::GET, Cow::Borrowed(&uri), Version::HTTP_11, Cow::Borrowed(&header_map));
+
+    let mut req_head_parse = RequestHeadParse::new(8096,header_map.len());
+
+    let mut cursor = std::io::Cursor::new(req_head.to_vec().unwrap());
+    
+    let _ = req_head_parse.read_data(&mut cursor);
+    let req_header_recv = req_head_parse.try_take_head().unwrap();
+
+    assert_eq!(req_head.method, req_header_recv.method);
+    assert_eq!(req_head.uri, req_header_recv.uri);
+    assert_eq!(req_head.version, req_header_recv.version);
+    assert_eq!(req_head.headers, req_header_recv.headers);
+
 }
