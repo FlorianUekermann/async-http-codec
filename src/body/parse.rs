cc5917335dafd2f37c9fc6f688ad1c62a99d4f95
Frangen, Leon <lesoftware@proton.me>
2024-05-26 17:22:46 +0200

    Add BodyParseChunked

--- src/body/parse.rs
+++ src/body/parse.rs
@@ -6,53 +6,38 @@ enum ReadState {
 }
 
 struct BodyParseChunked {
-    overlap: u32,
-    n: u32,
+    overlap: usize,
+    n: usize,
     state: ReadState,
 }
 
 impl BodyParseChunked {
     pub fn new() -> Self {
-	BodyParseChunked { overlap: 0, n: 0, state: ReadState::ReadLine }	
+	BodyParseChunked { overlap: 0, n: 0, state: ReadState::ReadLength }	
     }
     pub fn process_data<T: Read>(&mut self, rd: &mut T, out: &mut [u8])  -> Result<usize, std::io::Error> {
 	let mut read_until_term = [0u8;3];
+	let out_len = out.len();
 	loop {
 	    match self.state {
 		ReadState::ReadLength => {
-		    let mut bytes_read = 0;
-		    
 		    let next_bytes = self.guaranteed_read();
 		    if next_bytes == 0 {
 			self.overlap = 0;
 			self.state = ReadState::ReadLine;
-			return Ok(bytes_read)
+			continue;
 		    }
 		    
-		    rd.read_exact(&mut read_until_term[0..next_bytes])?;
-		    let mut iter = read_until_term.into_iter();
+		    let n_max = rd.read(&mut read_until_term[0..next_bytes])?;
+		    let mut iter = read_until_term[0..n_max].into_iter();
 	    
-		    while let Some(byte) = iter.next() {
-			bytes_read += 1;
-			
+		    while let Some(&byte) = iter.next() {
 			match byte as char {
-			    '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '0' | 'A' | 'B' | 'C' | 'D' | 'E' | 'F' => {
-				self.n = self.n*10 + (byte as char).to_digit(10).unwrap();
+			    '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '0' | 'A' | 'B' | 'C' | 'D' | 'E' | 'F' if self.overlap == 0 => {
+				self.n = self.n*16 + (byte as char).to_digit(16).unwrap() as usize;
 			    }
 			    '\r' => {
-				match iter.next()  {
-				    // 10u8 == '\n' as u8
-				    Some(10u8) => {
-					bytes_read += 1;
-					self.overlap = 2;
-				    },
-				    None => {
-					self.overlap = 1;
-				    },
-				    _ => {
-					return Err(std::io::Error::new(std::io::ErrorKind::InvalidData, "Error after \r while reading Content-Length Header"));
-				    }
-				}
+				self.overlap = 1;
 			    },
 			    '\n' if self.overlap == 1 => {
 				self.overlap = 2;
@@ -64,20 +49,30 @@ impl BodyParseChunked {
 		    }
 		}
 		ReadState::ReadLine => {
-		    rd.read_exact(&mut out[0..self.n as usize])?;
-		    let next_bytes = self.guaranteed_read();
-		    rd.read_exact(&mut read_until_term[0..next_bytes])?;
-		    match &read_until_term[0..2] {
-			// 13u8 == '\r' as u8
-			// 10u8 == '\n' as u8
-			[13u8, 10u8] => {
-			    self.state = ReadState::ReadLength;
-			    return Ok(self.n as usize + 2);
-			}
-			_ => {
-			    return Err(std::io::Error::new(std::io::ErrorKind::InvalidData, "No CRLF after reading Content"));			    
+		    
+		    let next_bytes = self.guaranteed_read().min(self.n);
+		    let bytes_read = rd.read(&mut out[0..next_bytes.min(out_len) as usize])?;
+		    self.n -= bytes_read;
+
+		    if self.n == 0 {
+		    
+			let next_bytes = self.guaranteed_read();
+			let n_reads = rd.read(&mut read_until_term[0..next_bytes])?;
+			let mut iter = read_until_term[0..n_reads].into_iter();
+
+			while let Some(&byte) = iter.next()  {
+			    match byte as char {
+				'\r' => self.overlap = 1,
+				'\n' if self.overlap == 1 => {
+				   *self = BodyParseChunked::new();
+				}
+				_ => {
+				    return Err(std::io::Error::new(std::io::ErrorKind::InvalidData, "No CRLF after reading Content"));
+				}
+			    }
 			}
 		    }
+		    return Ok(bytes_read as usize);
 		}
 	    }
 	}
@@ -85,10 +80,10 @@ impl BodyParseChunked {
     /// returns min number of unprocessed bytes (0 if done)
     pub fn guaranteed_read(&self) -> usize {
 	match self {
-	    BodyParseChunked { overlap: 0, n: 0, state: ReadState::ReadLine } => { 3 }
-	    BodyParseChunked { overlap: 0, n: _, state: ReadState::ReadLine } => { 2 }
-	    BodyParseChunked { overlap: 2, n: _, state: ReadState::ReadLine } => { 0 }
-	    BodyParseChunked { overlap: _, n: _, state: ReadState::ReadLength } => { self.n as usize }
+	    BodyParseChunked { overlap: 0, n: 0, state: ReadState::ReadLength } => { 3 }
+	    BodyParseChunked { overlap: 0, n: _, state: ReadState::ReadLength } => { 2 }
+	    BodyParseChunked { overlap: 2, n: _, state: ReadState::ReadLength } => { 0 }
+	    BodyParseChunked { overlap: _, n: _, state: ReadState::ReadLine } => { self.n + 2 - self.overlap}
 	    _ => { 1 }
 	}
     }
